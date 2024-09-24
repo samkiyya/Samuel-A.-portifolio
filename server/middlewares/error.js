51c10f6d4f4bfc8456631ac49b942d9882dadc50
samkiyya <samuelkiyya2012@gmail.com>
2024-09-24 10:04:10 -0700

    fix error midleware

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,31 +1,32 @@
-class ErrorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
+  // Handle MongoDB duplicate key error
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "JsonWebTokenError") {
-    const message = `Json Web Token is invalid, Try again!`;
-    err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "TokenExpiredError") {
-    const message = `Json Web Token is expired, Try again!`;
-    err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "CastError") {
-    const message = `Invalid ${err.path}`,
-      err = new ErrorHandler(message, 400);
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400); // Assign the new ErrorHandler instance to 'err'
   }
 
+  // Handle invalid JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = "Json Web Token is invalid, Try again!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Handle expired JWT error
+  if (err.name === "TokenExpiredError") {
+    const message = "Json Web Token is expired, Try again!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Handle Mongoose cast error (invalid ObjectId)
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Create a comprehensive error message
   const errorMessage = err.errors
     ? Object.values(err.errors)
         .map((error) => error.message)
@@ -37,5 +38,3 @@ export const errorMiddleware = (err, req, res, next) => {
     message: errorMessage,
   });
 };
-
-export default ErrorHandler;
