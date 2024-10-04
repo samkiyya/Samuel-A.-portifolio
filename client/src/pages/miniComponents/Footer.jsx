99e85de477c05c6d165dfc2d6cb8e4cb139b487e
Samuel Aberra <122904898+samkiyya@users.noreply.github.com>
2024-10-03 23:24:10 -0700

    Update Footer.jsx
    text updated

--- client/src/pages/miniComponents/Footer.jsx
+++ client/src/pages/miniComponents/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="p-5 mt-6 w-full max-w-[1050px] mx-auto">
       <hr />
       <h1 className="text-tubeLight-effect text-3xl mt-5 justify-center sm:justify-start tracking-[8px]">
-        Thanks For Visiting my Portifolio
+        Thanks For Visiting my Portfolio
       </h1>
     </footer>
   );
